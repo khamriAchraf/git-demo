9852c0090fe853157e67e12dad44c34ce4e850dd
khamriAchraf <achrafkhamri99@gmail.com>
2025-06-20 14:00:19 +0100

    final feature A update

--- ProjectA/src/FeatureA.java
+++ ProjectA/src/FeatureA.java
@@ -1,11 +1,10 @@
 public class FeatureA {
-
     public FeatureA() {
         System.out.println("FeatureA initialized.");
     }
 
     public void execute() {
-        System.out.println("Executing new FeatureA...");
+        System.out.println("Executing new Feature A...");
     }
 
     public static void main(String[] args) {
